ac83ac2d3e06c82e7da8a78e0279d6cbec2ef706
David Curtis <davidbradlycurtis@gmail.com>
2024-05-21 20:49:37 -0500

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var state: String
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Gets input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	if is_on_floor():
+		if direction == 0:
+			if Input.is_action_pressed("down"):
+				animated_sprite.play("crouch")
+				state = "crouch"
+			else:
+				animated_sprite.play("idle")
+				state = "idle"
+		else:
+			if Input.is_action_pressed("down"):
+				animated_sprite.play("sneak")
+				state = "sneek"
+			else:
+				animated_sprite.play("run")
+				state = "run"
+	else:
+		if velocity.y > 0:
+			animated_sprite.play("fall")
+			state = "fall"
+		else:
+			animated_sprite.play("jump")
+			state = "jump"
+
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	print(state)
+	move_and_slide()
